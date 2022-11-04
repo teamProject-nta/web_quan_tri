8303b9c24872f013c1a1c2e76a1111fea0b7ed91
NguyenAnh003 <teamprojectnta@gmail.com>
2022-11-04 18:00:15 +0700

    update_21

--- public/afterlogin/js/firebase.js
+++ public/afterlogin/js/firebase.js
@@ -1,17 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhECFBJadJ4yFQF4KxbVfA7a6mrWsM_jo",
+  authDomain: "appsale-ba3f9.firebaseapp.com",
+  databaseURL: "https://appsale-ba3f9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "appsale-ba3f9",
+  storageBucket: "appsale-ba3f9.appspot.com",
+  messagingSenderId: "757083996678",
+  appId: "1:757083996678:web:258b05b355945f9d207987",
+  measurementId: "G-85H2Z7X2MT"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBhECFBJadJ4yFQF4KxbVfA7a6mrWsM_jo",
-    authDomain: "appsale-ba3f9.firebaseapp.com",
-    databaseURL: "https://appsale-ba3f9-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "appsale-ba3f9",
-    storageBucket: "appsale-ba3f9.appspot.com",
-    messagingSenderId: "757083996678",
-    appId: "1:757083996678:web:258b05b355945f9d207987",
-    measurementId: "G-85H2Z7X2MT"
-  };
-
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const firestore = firebase.firestore();
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
